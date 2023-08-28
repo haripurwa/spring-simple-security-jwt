7cf03ff587779dca20c914f0a561c071eb6a7347
HariPurwanto <haripurwanto446@gmail.com>
2023-08-28 09:44:23 +0700

    first commit

---
+++ src/main/java/com/bumen/wetan/WetanApplication.java
@@ -0,0 +1,13 @@
+package com.bumen.wetan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumen/wetan/WetanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumen.wetan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
